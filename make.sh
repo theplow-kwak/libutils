0b1a65d52e2a81ba1916a45e2e9baab4010f555f
js kwak <theplow@naver.com>
2025-12-12 13:20:29 +0900

    Refactor make.sh to create output directory before compilation and improve output path handling

--- make.sh
+++ make.sh
@@ -5,9 +5,14 @@ fname=$(echo "$PARAM" | cut -d'.' -f1)
 ext=$(echo "$PARAM" | cut -s -d'.' -f2)
 ext=${ext:-"cpp"}
 
+files=""
+
 if [ "$fname" = "offset2lba" ]; then
     files="offset2lba_linux.cpp"
 fi
 
-echo g++ -std=c++2a -Wall -g -O0 -static -o $fname $fname.$ext $files
-g++ -std=c++2a -Wall -g -O0 -static -o $fname $fname.$ext $files
+outdir="build"
+mkdir -p "$outdir"
+
+echo g++ -std=c++2a -Wall -g -O0 -static -o "$outdir/$fname" "$fname.$ext" $files
+g++ -std=c++2a -Wall -g -O0 -static -o "$outdir/$fname" "$fname.$ext" $files
